d540d604dbb57f681216b7f59e1cf25f3176b144
JMarcley <joshua.marcley@gmail.com>
2014-08-28 14:56:51 -0700

    locations of subs are dynamic and dependent on center position

--- js/full-screen.js
+++ js/full-screen.js
@@ -24,11 +24,60 @@ function onScreenLoad() {
       }).appendTo("#debate-web-full");
     })
 
+
   var num_subs = web_data.claims.length;
 
-  console.log(num_subs)
+  if (num_subs < 6) {
+    var lengthTuner = 0.35;
+    var width = $("#debate-web-full").width();
+    var height = $("#debate-web-full").height();
+    var unitLocation = webLocation(num_subs);
+
+    $.each( web_data.claims, function(index) {
+
+      var left = width * 0.5 + width * lengthTuner * unitLocation[index][0];
+      var top = height * 0.5 + height * lengthTuner * unitLocation[index][1];
+
+
+
+      $("#subclaim-" + index).css({
+
+                                   width: function ( index, value ) {
+                                     return 200;
+                                   },
+                                   left: function ( index, value ) {
+                                     return left - parseFloat( $(this).css("width") ) / 2;
+                                   },
+                                   top: function () {
+                                     return top - parseFloat( $(this).css("height") ) / 2;
+                                   }
+                                  });
+    });
+  }
+
+  console.log(width, height);
 
   });
 
 
+}
+
+function sizeOfSub($obj) {
+  var textLength = $obj.text().length
+
+
+}
+
+function webLocation(count) {
+
+  var locations = new Array(count);
+  var offset = Math.random() - 0.5;
+
+  for ( i = 0; i < count; i++ ) {
+    locations[i] = new Array(2);
+    locations[i][0] = Math.sin( Math.PI * 2 / count * ( i + offset) );      //xloc
+    locations[i][1] = Math.cos( Math.PI * 2 / count * ( i + offset) );     //yloc
+  }
+
+  return locations;
 }
